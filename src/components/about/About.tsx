27122dd727cf2b8cd65306867c3c1f5499282964
BenMukebo <l.benkasmukebo7@gmail.com>
2023-03-14 00:41:18 +0200

    ak improvement at all

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import image1 from '../../assets/images/golden-monkeys.png';
+import image1 from '../../assets/images/gorilla_trek.png';
+import image2 from '../../assets/images/about_us.png';
+
 
 import styles from './about.module.scss';
 
@@ -12,11 +14,21 @@ export const About: React.FC<{}> = () => {
           <div className={styles.article_content}>
             <h2 className={styles.article_title}>Explore an untamed Zambia safari</h2>
             <p className={styles.article_text}>
-              A safari in Zambia will see you travel from Livingstone, the country’s adventure capital where the Zambezi and Victoria Falls – or Mosi-oa-Tunya – provide endless thrills, to the exceptionally remote Busanga Plains in northern Kafue National Park. At 5.5 million acres, Kafue is one of the largest and oldest protected regions in Africa, and it is here you’ll thrill to encountering wildlife in the most natural, wild way.
-            </p>
+            Gorilla Trekking safaris are second to none. When we put together a trip for you, we do so with the excitement of our own personal experiences and drawing on our memories to help create your own ultimate wildlife adventure encounter of a lifetime. Every one of our team of professionals and highly experienced guides is totally dedicated to making your dreams come true.
+            On our gorilla safaris in Uganda, Kawira Safaris can secure your tracking permit all year round (which not all tour operators can promise) and we are renowned for obtaining them quickly. We cover the Whole East Africa and with our extensive experience selling and leading tours either guided or Self drive            </p>
           </div>
           <div className={styles.article_img}>
             <img src={image1} alt="about-img1" />
+          </div>
+        </article>
+        <article className={styles.article}>
+          <div className={styles.article_content}>
+            <h2 className={styles.article_title}>Discover the mighty Zambezi</h2>
+            <p className={styles.article_text}>
+            We have been guiding Gorilla Trekking Safaris in Uganda and Rwanda, as well Wildlife Adventures for the last 10 years. Our company is founded on a true passion for the magnificent and diverse wildlife that lives in the protected National Parks, Rain Forests, and open plains of East Africa. Backed by a very clear understanding of what it means to deliver first-class service.</p>
+          </div>
+          <div className={styles.article_img}>
+            <img src={image2} alt="about-us" />
           </div>
         </article>
       </div>
--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -7,12 +7,15 @@ import styles from './contacts.module.scss';
 export const Contacts: React.FC<{}> = () => {
   return (
     <section className={styles.contacts_section} id="contacts">
-        <div className={styles.contacts_header}>
-          <h2 className={styles.contacts_title}>Need some inspiration? </h2>
-        </div>
+      {/* <div className={styles.contacts_header}>
+        <h2 className={styles.contacts_title}>Need some inspiration? </h2>
+      </div> */}
       <div className={styles.contacts_wrapper}>
         <article className={styles.contact_content}>
-          <p>
+        <div className={styles.contacts_header}>
+          <h2 className={styles.contacts_title}>Need some <br /> inspiration? </h2>
+        </div>
+          <p className={styles.contacts_text}>
             Be inspired by the latest news from Wilderness. Subscribe to our newsletter.
           </p>
         </article>
