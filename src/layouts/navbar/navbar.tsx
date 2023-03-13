717b90bdc1c29c922d67ab8a9596960fff307960
BenMukebo <l.benkasmukebo7@gmail.com>
2023-03-13 12:30:25 +0200

    Update top navbar content

--- src/layouts/navbar/navbar.tsx
+++ src/layouts/navbar/navbar.tsx
@@ -2,8 +2,7 @@ import React from "react";
 
 import { FaFacebookF } from 'react-icons/fa';
 import { AiOutlineTwitter, AiOutlineDribbble } from 'react-icons/ai';
-import { TbBrandInstagram } from 'react-icons/tb';
-import { BiPaperPlane } from 'react-icons/bi';
+import { BiPaperPlane, BiSearchAlt2 } from 'react-icons/bi';
 
 import styles from './navbar.module.scss';
 
@@ -11,31 +10,24 @@ export const Navbar: React.FC<{}> = ( ) => {
 
   return (
     <nav className={styles.navbar}>
-      <div className={styles.contact_info}>
-       <p className={styles.contact_number}>+243 994 764 613</p>
-       <p className={styles.contact_email}> 
-       <span><BiPaperPlane/></span>
-       info@sateccongo.com</p>
-      </div>
+        <p className={styles.contact_numbers}>Call Now: +250 787 976 905 | +250 787 976 905</p>
+        <div className={styles.contact_email_social_left}>
+          <p className={styles.contact_email}>
+            <span><BiPaperPlane/></span>
+            Email: info@kawirasafaris.net.com
+          </p>
 
-      <ul className={styles.social_media_wrapper}>
-        <li className={styles.social_media}>
-          <a href="https://www.facebook.com/pages/category/Business-Service/SATEC-Congo-incubation-113625197074726/" title="facebook">
-            <FaFacebookF/>
-          </a>
-        </li>
-        <li className={styles.social_media}>
-          <a href="https://www.facebook.com/satecongo?mibextid=LQQJ4d" title="twitter">
-            <AiOutlineTwitter/>
-          </a>
-        </li>
-        <li className={styles.social_media}>
-          <a href="https://instagram.com/sateccongo?igshid=YWJhMjlhZTc=" title="instagram"><TbBrandInstagram/></a>
-        </li>
-        <li className={styles.social_media}>
-          <a href="https://wa.me/+243971838655" title="whatsApp"><AiOutlineDribbble/></a>
-        </li>
-      </ul>
+          <p className={styles.social_media}>
+            <span className={styles.follow}>Follow Us: </span>
+            <a href="https://www.facebook.com/pages/category/Business-Service/SATEC-Congo-incubation-113625197074726/" title="facebook">
+              <FaFacebookF/>
+            </a>
+            <a href="https://www.facebook.com/satecongo?mibextid=LQQJ4d" title="twitter">
+              <AiOutlineTwitter/>
+            </a>
+          </p>
+
+        </div>
     </nav> 
   )
 }
