1f034769f0ed9929a57a1ad122079c1eb75f51e3
BenMukebo <l.benkasmukebo7@gmail.com>
2023-03-14 17:09:07 +0200

    Update config functio styles

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,6 @@
-// export { Home } from './Home';
 export * from './intro/Intro';
 export * from './about/About';
 export * from './services/Services';
-// export * from './products/Products';
 export * from './team/Team';
-// export * from './testimonials/Testimonials';
 export * from './features/Features';
 export * from './contacts/Contacts';
