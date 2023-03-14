b65f55d5233bf8611dcf738048474a3687498fba
BenMukebo <l.benkasmukebo7@gmail.com>
2023-03-14 17:02:45 +0200

    Create gorille components

---
+++ src/pages/gorilla/gorilla.tsx
@@ -0,0 +1,13 @@
+import { Main } from '../../layouts';
+import {  Contacts  } from '../../components/index';
+// import styles from './gorilla.module.scss';
+
+export const Gorilla: React.FC<{}> = () => {
+  return (
+    <>
+      <Main>
+        <Contacts />
+      </Main>
+    </>
+  );
+}
